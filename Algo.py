1364802e10f0d134102cdacf4c1d8827108bada0
ThanhPhuc18 <vtphuc21@apcs.fitus.edu.vn>
2023-11-27 18:02:29 +0700

    Xong BFS, fix het loi

--- Algo.py
+++ Algo.py
@@ -18,11 +18,12 @@ def bfs(world):
             continue
 
         visited.add(pos)
+        directions = [(0, 1), (0, -1), (1, 0), (-1, 0), (1, 1), (-1, -1), (-1, 1), (1, -1)]
 
-
-        for hori, verti in [(0, 1), (0, -1), (1, 0), (-1, 0)]:#Not checking the diagonal movement
+        for hori, verti in directions:
             new_pos = [pos[0] + verti, pos[1] + hori]
 
+            # Check if the move is valid using the move function
             if world.move(verti, hori):
                 new_path = path + [(new_pos, world._check(verti, hori))]
                 queue.append((new_pos, new_path))
